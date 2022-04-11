23ad3782ff98b19af0ae9773c9b08f12f80badfe
Diego Torres <torressdiiego@outlook.com>
2022-04-11 20:58:20 -0300

    Estudos do dia 11/04/2022

---
+++ ProjetoFinal/index.php
@@ -0,0 +1,29 @@
+<?php
+
+use Hcode\PageAdmin;
+use Slim\Slim;
+use Hcode\Page;
+
+require_once("vendor/autoload.php");
+
+$app = new Slim();
+
+$app->config('debug', true);
+
+$app->get('/', function() {
+
+    $page = new Page();
+    $page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+
+    $page = new PageAdmin();
+    $page->setTpl("index");
+
+});
+
+$app->run();
+
+ ?>
